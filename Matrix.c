190e8bc119ec721932ac53fa137177064da9d359
Samuel Tocci <samuel.tocci@student.kuleuven.be>
2023-02-24 12:17:38 +0100

    Matrix data types + free + padding start

--- Matrix.c
+++ Matrix.c
@@ -1,14 +1,16 @@
 #define _GNU_SOURCE
 #include "Matrix.h"
+#include <stdlib.h>
+#include <stdint.h>
 
 
 struct matrix{
-    int x;
-    int y;
-    int *data;
+    uint16_t x; //2 byte
+    uint16_t y; //2 byte
+    int *data; //TODO: define exact data type for efficiency
 };
 
-matrix_t* matrix_create(int x, int y, int *data){
+matrix_t* matrix_create(int x, int y, int data[]){
     matrix_t * matrix;
     matrix = malloc(sizeof(struct matrix));
     matrix->x = x;
@@ -20,4 +22,14 @@ matrix_t* matrix_create(int x, int y, int *data){
 void matrix_free(matrix_t **matrix){
     free((*matrix)->data);
     free(*matrix);
+    *matrix = NULL;
+}
+
+matrix_t* matrix_padStart(matrix_t **matrix, int amount){
+    return matrix;
+}
+
+matrix_t* matrix_padEnd(matrix_t **matrix, int amount){
+    (*matrix)->data = realloc(sizeof((*matrix)->data)+amount);
+    return matrix;
 }
--- Matrix.h
+++ Matrix.h
@@ -1,7 +1,16 @@
 #ifndef _MATRIX_H_
 #define _MATRIX_H_
 
-typedef struct matrix matrix_t
+typedef struct matrix matrix_t;
+
+matrix_t* matrix_create(int x, int y, int *data);
+
+void matrix_free(matrix_t **matrix);
+
+matrix_t* matrix_padStart(matrix_t **matrix, int amount);
+
+matrix_t* matrix_padEnd(matrix_t **matrix, int amount);
+
 
 
 #endif //_MATRIX_H_
